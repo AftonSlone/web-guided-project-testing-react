86e77f958389d05b5ca1c262158624cdd8c1aa37
Dustin Myers <dustint314@gmail.com>
2021-02-22 11:21:32 -0700

    wip

--- src/components/MissionsForm.test.js
+++ src/components/MissionsForm.test.js
@@ -3,6 +3,8 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import MissionForm from "./MissionForm";
 
+// mock functions being passed to the comp
+
 test("shows loading message when button is clicked", () => {
   render(<MissionForm />);
 

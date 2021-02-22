319bb558ab4cd8fc1511cee6de661ee911b59ce5
Dustin Myers <dustint314@gmail.com>
2021-02-22 10:26:12 -0700

    wip

--- src/components/MissionsList.test.js
+++ src/components/MissionsList.test.js
@@ -10,7 +10,7 @@ test("re-render properly with missions data", () => {
   render(<MissionsList missions={[]} />);
 
   // first assert there are NO missions in the UI
-  expect(screen.queryByText(/thaicom/i)).not.toBeNull();
+  expect(screen.queryByText(/thaicom/i)).toBeNull();
 
   // re-render comp with new props
 

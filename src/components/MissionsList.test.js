a6f4b8bbac258cd6c1d2ac4526521a4dff5a9934
Dustin Myers <dustint314@gmail.com>
2021-02-22 11:34:39 -0700

    wip

--- src/components/MissionsList.test.js
+++ src/components/MissionsList.test.js
@@ -52,7 +52,7 @@ test("renders without errors", () => {
 });
 
 test("re-render properly with missions data", () => {
-  const { rerender } = render(<MissionsList missions={[]} />);
+  const { rerender } = render(<MissionsList />);
 
   // first assert there are NO missions in the UI
   expect(screen.queryByText(/thaicom/i)).toBeNull();

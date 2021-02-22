c47ccf71a7be6c5908f16db831b0a05d748b3290
Dustin Myers <dustint314@gmail.com>
2021-02-22 11:50:55 -0700

    wip

--- src/App.test.js
+++ src/App.test.js
@@ -2,3 +2,7 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import App from "./App";
+
+test("render without errors", () => {
+  render(<App />);
+});
